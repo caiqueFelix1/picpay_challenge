c6a1773b5259aa024cdc302bab5e386a209f0205
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-03-23 17:15:56 -0300

    :tada: init commit

---
+++ src/test/java/br/com/challengegd/challenge_picpay/ChallengePicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challengegd.challenge_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengePicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
